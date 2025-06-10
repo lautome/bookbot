840b64fbd37a350ce4cc6d42c08fee50f1c7aaee
b <b@b.com>
2025-06-10 14:29:41 +0200

    feat: count words

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def get_book_text(filepath):
         string = f.read()
         return string
 
+def count_words(filecontent):
+    list = filecontent.split()
+    return len(list)
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-
+    filecontent = get_book_text("books/frankenstein.txt")
+    print(f"{count_words(filecontent)} words found in the document")
 main()
